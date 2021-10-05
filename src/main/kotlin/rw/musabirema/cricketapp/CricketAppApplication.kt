a85c3852c7ed91521af39b0fd02237056ccbcd8c
mmusabirema <mmusabirema@bk.rw>
2021-10-05 03:39:19 +0200

    initial project

---
+++ src/main/kotlin/rw/musabirema/cricketapp/CricketAppApplication.kt
@@ -0,0 +1,11 @@
+package rw.musabirema.cricketapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CricketAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<CricketAppApplication>(*args)
+}
---
+++ src/test/kotlin/rw/musabirema/cricketapp/CricketAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package rw.musabirema.cricketapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CricketAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
